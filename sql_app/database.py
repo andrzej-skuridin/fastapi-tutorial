43d5e52458fa0868eb58eb8d977ba0207549f7a2
Andrzej <andreyskuridin1995@gmail.com>
2023-12-04 12:29:00 +0300

    postgresql db

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pydantic import PostgresDsn
 
 # Opening a file with the SQLite database.
 # The file will be located at the same directory in the file sql_app.db.
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+#PostgreSQL:
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://core:wCh29&HE&T83@localhost/fastapi_tutorial_db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, 
+    # connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
